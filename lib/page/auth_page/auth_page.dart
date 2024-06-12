13622c383c51506817b8198ec33f86c0cb2fae3f
Adevalter <adevalter@hotmail.com>
2024-06-12 09:31:04 -0300

    implementado metodos para envio de dados formulario

--- lib/page/auth_page/auth_page.dart
+++ lib/page/auth_page/auth_page.dart
@@ -1,18 +1,39 @@
 import 'package:awchat/components/auth_form.dart';
+import 'package:awchat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool isLoading = false;
+  void _handleSubmit(AuthFormData _formData) {
+    setState(() => isLoading = true);
+    print(_formData.email);
+    setState(() => isLoading = false);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: const Center(
-        child: SingleChildScrollView(
-          child: AuthForm(),
+      body: Stack(children: [
+        Center(
+          child: SingleChildScrollView(
+            child: AuthForm(onSubmit: _handleSubmit),
+          ),
         ),
-      ),
+        if (isLoading == true)
+          Container(
+            decoration:
+                const BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.5)),
+            child: const Center(child: CircularProgressIndicator()),
+          )
+      ]),
     );
   }
 }
