09034cfd4cc45c864b1bdaf60125c38e686a6c01
Adevalter <adevalter@hotmail.com>
2024-06-13 12:35:08 -0300

    implementado envio de mensagem

--- lib/components/new_message.dart
+++ lib/components/new_message.dart
@@ -1,12 +1,46 @@
+import 'package:awchat/core/auth/auth_service.dart';
+import 'package:awchat/core/services/chat_service.dart';
 import 'package:flutter/material.dart';
 
-class NewMessage extends StatelessWidget {
+class NewMessage extends StatefulWidget {
   const NewMessage({super.key});
 
   @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  String message = '';
+  final _messageConstroller = TextEditingController();
+
+  Future<void> _sendMessage() async {
+    final user = AuthService().currentUser;
+    if (user != null) {
+      ChatService().save(message, user);
+    }
+    _messageConstroller.clear();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Nova Mensagem"),
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _messageConstroller,
+            onChanged: (msg) => setState(() => message = msg),
+            decoration: const InputDecoration(labelText: 'Enviar Mensagem ...'),
+            onSubmitted: (_) {
+              if (message.trim().isNotEmpty) {
+                _sendMessage();
+              }
+            },
+          ),
+        ),
+        IconButton(
+            onPressed: message.trim().isEmpty ? null : _sendMessage,
+            icon: Icon(Icons.send))
+      ],
     );
   }
 }
