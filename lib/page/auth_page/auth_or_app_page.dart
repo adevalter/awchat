6c20d83a371497b8587db2bff2c6a010229c7825
Adevalter <adevalter@hotmail.com>
2024-06-17 21:36:03 -0300

    implementado notificacao

--- lib/page/auth_page/auth_or_app_page.dart
+++ lib/page/auth_page/auth_or_app_page.dart
@@ -1,16 +1,19 @@
 import 'package:awchat/core/services/auth/auth_service.dart';
 import 'package:awchat/core/models/chat_user.dart';
+import 'package:awchat/core/services/notification/chat_notification_service.dart';
 import 'package:awchat/page/auth_page/auth_page.dart';
 import 'package:awchat/page/chat_page/chat_page.dart';
 import 'package:awchat/page/loading_page/loading_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({super.key});
 
   Future<void> init(BuildContext context) async {
     Firebase.initializeApp();
+    await Provider.of<ChatNotificationService>(context, listen: false).init();
   }
 
   @override
